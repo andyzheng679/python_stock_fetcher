2f585746e12352fc9356ccd259e06067ecd65122
Andy Zheng <andyzheng679@gmail.com>
2022-08-22 10:14:33 -0400

    testing

--- stock_fetcher.py
+++ stock_fetcher.py
@@ -1,8 +1,8 @@
 import requests
+import creds
 
-api_key = ""
 base_url = "https://api.twelvedata.com/time_series?"
 ticker = input("Enter ticker symbol you want to view:" )
-request_url = f"{base_url}symbol={ticker}&interval=1day&apikey={api_key}"
+request_url = f"{base_url}symbol={ticker}&interval=1day&apikey={creds.api_key}"
 data = requests.get(request_url).json()
 print(data["values"][0])            # prints most current data
