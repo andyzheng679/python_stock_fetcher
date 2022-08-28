ebc45f8fef53e391d9e96a6735ab81da5ef200e8
Andy Zheng <andyzheng679@gmail.com>
2022-08-28 17:17:00 -0400

    added user input checker, loops if entry is invalid

--- stock_fetcher.py
+++ stock_fetcher.py
@@ -1,8 +1,38 @@
-from tracemalloc import reset_peak
 import requests
 import creds
 
 base_url = "https://api.twelvedata.com/"
+
+
+class input_check:
+    def checker(start):
+        try:
+            user_input = int(start)
+            print("yes")
+        except ValueError:
+            print("Invalid Entry, not an int")
+
+while True:
+    start = input("Enter 1 to view Real-Time Data,\nEnter 2 to view Quotes,\nEnter 3 to view Time Series: ")
+
+    try:
+        if int(start) >= 4:
+            print("Invalid Entry, Try Again")
+            continue
+        else:
+            input_check.checker(start)
+    except ValueError:
+        input_check.checker(start)
+        continue
+
+
+    break
+
+
+
+
+
+
 
 # Real-Time Price
 
@@ -22,7 +52,7 @@ base_url = "https://api.twelvedata.com/"
 
 # Times Series
 
-ticker_symbol = input("Enter ticker symbol you want to view:" ).upper()
-ts_url = f"{base_url}time_series?symbol={ticker_symbol}&interval=1day&apikey={creds.api_key}"
-ts_data = requests.get(ts_url).json()
-print(ts_data["values"])            
+#ticker_symbol = input("Enter ticker symbol you want to view:" ).upper()
+#ts_url = f"{base_url}time_series?symbol={ticker_symbol}&interval=1day&apikey={creds.api_key}"
+#ts_data = requests.get(ts_url).json()
+#print(ts_data["values"])            
