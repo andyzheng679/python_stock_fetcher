741067a21cf008d0e9f3c642e01a952543885a9c
Andy Zheng <andyzheng679@gmail.com>
2022-10-03 00:06:44 -0400

    added page for those functions to display data, added html page to for said display

--- stock_fetcher_back/flask_front.py
+++ stock_fetcher_back/flask_front.py
@@ -1,4 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request
+from stock_fetcher import real_time_price
+from stock_fetcher import real_quotes
+from stock_fetcher import time_series
+
+
 
 app = Flask(__name__)
 
@@ -11,7 +16,7 @@ def home():
 def rtp():
     if request.method == "POST":
         ticker = request.form["rtp"]
-        return redirect(url_for("user", usr=user))
+        return redirect(url_for("display_rtp", usr=ticker))
     else:
         return render_template("rtp.html")
 
@@ -19,7 +24,7 @@ def rtp():
 def quotes():
     if request.method == "POST":
         ticker = request.form["quotes"]
-        return redirect(url_for("user", usr=user))
+        return redirect(url_for("display_quotes", usr=ticker))
     else:
         return render_template("quotes.html")
 
@@ -27,16 +32,32 @@ def quotes():
 def ts():
     if request.method == "POST":
         ticker = request.form["ts"]
-        return redirect(url_for("user", usr=user))
+        return redirect(url_for("display_ts", usr=ticker))
     else:
         return render_template("ts.html")
 
 
 
+@app.route("/<usr>_real_time_price")
+def display_rtp(usr):
+    data = real_time_price().rtp(usr)
+    return f"<center><h1>{usr}</h1><h2>{data}</h2></center>"
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/<usr>_real_quotes")
+def display_quotes(usr):
+    data = real_quotes().test_quotes(usr)
+    return render_template("display_quotes.html",data=data,usr=usr)
+
+
+@app.route("/<usr>_time_series")
+def display_ts(usr):
+    data = time_series().ts(usr)
+    return render_template("display_ts.html",data=data,usr=usr)
+    
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- stock_fetcher_back/stock_fetcher.py
+++ stock_fetcher_back/stock_fetcher.py
@@ -5,89 +5,76 @@ import pandas as pd
 
 base_url = "https://api.twelvedata.com/"
 
-class input_check:
-    def checker(start):
-        try:
-            user_input = int(start)
-            if int(start) == 1:
-                real_time_price.rtp_display(start)
-            if int(start) == 2:
-                quotes.quote_display(start)
-            if int(start) == 3:
-                time_series.ts_display(start)
-        except ValueError:
-            print("Invalid Entry, Try Again")
-
 class real_time_price:
-    def rtp_display(start):
-        ticker_symbol = input("Enter ticker symbol you want to view: ").upper()
-        rtp_url = f"{base_url}price?symbol={ticker_symbol}&apikey={creds.api_key}"
+    def rtp(self, ticker):
+        
+        ticker = ticker.upper()
+        rtp_url = f"{base_url}price?symbol={ticker}&apikey={creds.api_key}"
         rtp_data = requests.get(rtp_url).json()
+        return rtp_data
+        
 
-        try:
-            if rtp_data['code'] == 400:
-                print("Invalid Symbol")
-        except KeyError:
-            print(ticker_symbol)
-            print(rtp_data)
-            
-class quotes:
-    def quote_display(start):
-        ticker_symbol = input("Enter ticker symbol you want to view: ").upper()
-        quote_url = f"{base_url}quote?symbol={ticker_symbol}&apikey={creds.api_key}"
+class real_quotes:
+    def test_quotes(self, ticker):
+        
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
         quote_data = requests.get(quote_url).json()
-        #data2 = pd.DataFrame(quote_data)
+        return quote_data
 
-        # try:
-        #     if quote_data['code'] == 400:
-        #         print("Invalid Symbol")
-        # except KeyError:
-        #     print()
+    def quote_open(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "Open : %s" %  quote_data.get('open')
+    
+    def quote_high(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "High : %s" %  quote_data.get('high')
 
-        print("symbol : %s" %  quote_data.get('symbol'))
-        print("name : %s" %  quote_data.get('name'))
-        print("exchange : %s" %  quote_data.get('exchange'))
-        print("datetime : %s" %  quote_data.get('datetime'))
-        print("open : %s" %  quote_data.get('open'))
-        print("high : %s" %  quote_data.get('high'))
-        print("low : %s" %  quote_data.get('low'))
-        print("close : %s" %  quote_data.get('close'))
-        print("volume : %s" %  quote_data.get('volume'))
-        print("previous close : %s" %  quote_data.get('previous_close'))
-        print("change : %s" %  quote_data.get('change'))
-        print("percent change : %s" %  quote_data.get('percent_change'))
-        print("average volume : %s" %  quote_data.get('average_volume'))
-        print("fifty-two week : %s" %  quote_data.get('fifty_two_week'))
+    def quote_low(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "Low : %s" %  quote_data.get('low')
+    
+    def quote_close(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "Close : %s" %  quote_data.get('close')
+
+    def quote_volume(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "Volume : %s" %  quote_data.get('volume')
+    
+    def quote_pre_close(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "Previous close : %s" %  quote_data.get('previous_close')
+    
+    def quote_avg_vol(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "Average volume : %s" %  quote_data.get('average_volume')
+    
+    def quote_fifty_two_high(self, ticker):
+        ticker = ticker.upper()
+        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
+        quote_data = requests.get(quote_url).json()
+        return "Fifty-two week : %s" %  quote_data.get('fifty_two_week')
 
 class time_series:
-    def ts_display(start):
-        ticker_symbol = input("Enter ticker symbol you want to view:" ).upper()
-        ts_url = f"{base_url}time_series?symbol={ticker_symbol}&interval=1day&apikey={creds.api_key}"
+    def ts(self, ticker):
+        
+        ticker = ticker.upper()
+        ts_url = f"{base_url}time_series?symbol={ticker}&interval=1day&apikey={creds.api_key}"
         ts_data = requests.get(ts_url).json()
-        data3 = pd.DataFrame(ts_data["values"])
-        
-        try:
-            if ts_data['code'] == 400:
-                print("Invalid Symbol")
-        except KeyError:
-            print(data3)
-
-
-
-while True:
-    start = input("Enter 1 to view Real-Time Data,\nEnter 2 to view Quotes,\nEnter 3 to view Time Series: ")
-    
-    
-    try:
-        if int(start) >= 4:
-            print("Invalid Entry, Try Again")
-            continue
-        elif int(start) == 0:
-            break
-        else:
-            input_check.checker(start)
-    except ValueError:
-        input_check.checker(start)
-        continue
-
-        
+        data2 = pd.DataFrame(ts_data["values"])
+        return data2
