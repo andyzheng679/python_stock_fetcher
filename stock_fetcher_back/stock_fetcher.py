18f8926faf25c61b8fb21e1d1092e1f93f7c9d6a
Andy Zheng <andyzheng679@gmail.com>
2022-10-03 00:16:16 -0400

    readme

--- stock_fetcher_back/stock_fetcher.py
+++ stock_fetcher_back/stock_fetcher.py
@@ -22,53 +22,7 @@ class real_quotes:
         quote_data = requests.get(quote_url).json()
         return quote_data
 
-    def quote_open(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "Open : %s" %  quote_data.get('open')
-    
-    def quote_high(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "High : %s" %  quote_data.get('high')
 
-    def quote_low(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "Low : %s" %  quote_data.get('low')
-    
-    def quote_close(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "Close : %s" %  quote_data.get('close')
-
-    def quote_volume(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "Volume : %s" %  quote_data.get('volume')
-    
-    def quote_pre_close(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "Previous close : %s" %  quote_data.get('previous_close')
-    
-    def quote_avg_vol(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "Average volume : %s" %  quote_data.get('average_volume')
-    
-    def quote_fifty_two_high(self, ticker):
-        ticker = ticker.upper()
-        quote_url = f"{base_url}quote?symbol={ticker}&apikey={creds.api_key}"
-        quote_data = requests.get(quote_url).json()
-        return "Fifty-two week : %s" %  quote_data.get('fifty_two_week')
 
 class time_series:
     def ts(self, ticker):
